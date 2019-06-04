17171392dfae24d87cd53cfdaa0a1595e86cd64f
sungwoo <su@naver.com>
2019-06-04 09:33:12 +0100

    <server clint>

--- server/sensor_server.c
+++ server/sensor_server.c
@@ -6,12 +6,15 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 #define PORT 20162
 #define BUFFER_SIZE 4096
 #define BUFF_SIZE 100
 
-#defien LISTEN_QUEUE_SIZE 5
+#define LISTEN_QUEUE_SIZE 5
 
 void childHandler(int signal)
 {
@@ -31,4 +34,71 @@ int main(){
 
 	listenSocket.sin_family = AF_INET;
 	listenSocket.sin_addr.s_addr = htonl(INADDR_ANY);
-	listenSockte.sin_port = htons(PORT);
+	listenSocket.sin_port = htons(PORT);
+
+	int listenFD = socket(AF_INET, SOCK_STREAM,0);
+	int connectFD;
+
+	ssize_t receivedBytes;
+	char readBuff[BUFFER_SIZE];
+	char sendBuff[BUFFER_SIZE];
+	pid_t pid;
+
+	if(bind(listenFD, (struct sockaddr *)&listenSocket, sizeof(listenSocket))==-1){
+		printf("Can not bind.\n");
+		return -1;
+	}
+
+	if(listen(listenFD, LISTEN_QUEUE_SIZE) == -1){
+		printf("Listen fail.\n");
+		return -1;
+	}
+
+	printf("Wating for clients....\n");
+
+	while(1){
+		struct sockaddr_in connectSocket, peerSocket;
+
+		socklen_t connectSocketLength = sizeof(connectSocket);
+
+		while((connectFD = accept(listenFD,(struct sockaddr*)&connectSocket,(socklen_t *)&connectSocketLength))>=0){
+			getpeername(connectFD,(struct sockaddr*)&peerSocket, &connectSocketLength);
+			char peerName[sizeof(peerSocket.sin_addr)+1]={0};
+			sprintf(peerName,"%s",inet_ntoa(peerSocket.sin_addr));
+
+			if(strcmp(peerName,"0.0.0.0")!=0){
+				printf("Client : %s\n",peerName);
+			}
+
+			if(connectFD<0){
+				printf("Server: accept failed\n");
+				exit(0);
+			}
+
+			if(pid==0){
+				close(listenFD);
+
+				ssize_t receivedBytes;
+
+				while((receivedBytes = read(connectFD,readBuff,BUFF_SIZE))>0){
+					readBuff[receivedBytes]='\0';
+					fputs(readBuff,stdout);
+					fflush(stdout);
+
+					sprintf(sendBuff,"%s",readBuff);
+					write(connectFD,sendBuff,strlen(sendBuff));
+				}
+
+				close(connectFD);
+				return 0;
+			}
+			else{
+				close(connectFD);
+			}
+		}
+	}
+	close(listenFD);
+	return 0;
+}
+
+
